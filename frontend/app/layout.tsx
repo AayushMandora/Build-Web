3ecdc04069bd0d6c29a4b86c9e4fe82a4e217575
AayushMandora <aayushmandora76@gmail.com>
2025-02-19 22:42:02 +0530

    Added favicon icon.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 import { Header } from "./components/header";
@@ -17,7 +18,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Build-Web",
   description:
-    "Build Website with Build-Web AI Tool. Website that build Website",
+    "Build Website with Build-Web AI Tool. Website that build Website. Made by Aayush Mandora",
 };
 
 export default function RootLayout({
@@ -27,6 +28,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <Head>
+        <link rel="icon" href="/logo.png" sizes="64" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
