26766806a3dbb7696bbb5561f1b4188b1416e006
AayushMandora <aayushmandora76@gmail.com>
2025-02-19 13:12:17 +0530

    Added Flow-Up prompt.

--- frontend/app/components/build-interface.tsx
+++ frontend/app/components/build-interface.tsx
@@ -1,28 +1,46 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import Split from "react-split";
 
 import axios from "axios";
+
+import { ArrowRight } from "lucide-react";
+import { motion } from "framer-motion";
+
+import { Oval } from "react-loader-spinner";
 
 import { parseXML, Step, StepType } from "@/utils/steps";
 import { FileItem } from "@/utils/types";
 import { transformFiles } from "@/utils/helper";
 import useWebContainer from "@/utils/webContainer";
 
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 import { Terminal } from "./terminal";
 import { CodePreview } from "./code-preview";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Preview from "./preview";
 
 export function BuildInterface({ prompt }: { prompt: string }) {
   const webContainer = useWebContainer();
 
+  const [loading, setLoading] = useState(false);
+
   const [steps, setSteps] = useState<Step[]>([]);
   const [files, setFiles] = useState<FileItem[]>([]);
+  const [flowUpPrompt, setFlowUpPrompt] = useState<string>("");
+
+  const [llmResponse, setLlmResponse] = useState<
+    {
+      role: "user" | "assistant";
+      parts: [{ text: string }];
+    }[]
+  >([]);
 
   // init
-  const init = async () => {
+  const init = useCallback(async () => {
     const response = await axios.post(
       `${process.env.NEXT_PUBLIC_BASE_URL}/template`,
       {
@@ -52,10 +70,25 @@ export function BuildInterface({ prompt }: { prompt: string }) {
     const result = chatResponse.data.result;
 
     setSteps((prevSteps) => [...prevSteps, ...parseXML(result)]);
-  };
+
+    setLlmResponse([
+      prompts.map((step: string) => {
+        return { role: "user", parts: [{ text: step }] };
+      }),
+      {
+        role: "user",
+        parts: [{ text: prompt }],
+      },
+    ]);
+
+    setLlmResponse((data) => [
+      ...data,
+      { role: "assistant", parts: [{ text: result }] },
+    ]);
+  }, []);
 
   // make fileStructure
-  const buildFileStructure = () => {
+  const buildFileStructure = useCallback(() => {
     let originalFiles = [...files];
     let updateHappened = false;
     steps
@@ -123,11 +156,40 @@ export function BuildInterface({ prompt }: { prompt: string }) {
         })
       );
     }
-  };
+  }, [steps]);
 
-  const processFiles = async () => {
+  // mount files to web container
+  const processFiles = useCallback(async () => {
     const result = transformFiles(files);
     await webContainer?.mount(result);
+  }, [files]);
+
+  const flowUp = async () => {
+    setLoading(true);
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/chat`,
+      {
+        messages: [
+          llmResponse,
+          {
+            role: "user",
+            parts: [{ text: flowUpPrompt }],
+          },
+        ],
+      }
+    );
+
+    const result = response.data.result;
+
+    setSteps((prevSteps) => [...prevSteps, ...parseXML(result)]);
+
+    setLlmResponse((data) => [
+      ...data,
+      { role: "assistant", parts: [{ text: result }] },
+    ]);
+
+    setFlowUpPrompt("");
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -155,7 +217,37 @@ export function BuildInterface({ prompt }: { prompt: string }) {
       className="flex h-[100%]"
     >
       <div className="h-full bg-black p-4">
-        <Terminal steps={steps} projectName={prompt} />
+        <Terminal steps={steps} />
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.6, duration: 0.5 }}
+          className="flex gap-4 justify-center items-center mt-3"
+        >
+          <Textarea
+            placeholder="Write your amazing thing..."
+            value={flowUpPrompt}
+            onChange={(e) => setFlowUpPrompt(e.target.value)}
+            rows={4}
+            className="resize-none bg-gradient-to-b from-black/10 to-transparent focus-visible:ring-0 "
+          />
+          <Button
+            type="submit"
+            size="sm"
+            className="h-12 px-4"
+            disabled={!flowUpPrompt.trim()}
+            onClick={flowUp}
+          >
+            {loading ? (
+              <Oval color="#fff" height={20} width={20} />
+            ) : (
+              <>
+                <ArrowRight className="mr-2 h-4 w-4" />
+                Send
+              </>
+            )}
+          </Button>
+        </motion.div>
       </div>
       <div className="h-full border-l">
         <Tabs defaultValue="code" className="h-full">
@@ -166,7 +258,7 @@ export function BuildInterface({ prompt }: { prompt: string }) {
             </TabsList>
           </div>
           <TabsContent value="preview" className="h-[calc(100%-48px)]">
-            <Preview webContainer={webContainer} />
+            <Preview webContainer={webContainer} files={files} />
           </TabsContent>
           <TabsContent value="code" className="h-[calc(100%-48px)] p-4">
             <CodePreview fileStructure={files} />
